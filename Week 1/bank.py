5c31215da5c5f69b5fd6fbac36f2f7f7b9301215
PIYUSH GARG <piyushgargxd@gmail.com>
2026-05-28 11:46:27 +0530

    Delete Week 1 directory

--- Week 1/bank.py
+++
@@ -1,8 +0,0 @@
-greet = input("Greetings: ").lower().strip()
-
-if greet.startswith("hello"):
-    print("$0")
-elif greet.startswith("h"):
-    print("$20")
-else:
-    print("$100")
--- Week 1/deep.py
+++
@@ -1,8 +0,0 @@
-deep = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-deep = deep.lower().strip()
-
-match deep:
-    case "42" | "forty two" | "forty-two" :
-        print("Yes")
-    case _:
-        print("No")
